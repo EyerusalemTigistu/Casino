2b5df4424eb91bf46603e4ba2f202f72dfe45bc2
tes-Ah <ahmedtesnim@gamil.com>
2025-07-09 20:51:58 +0300

    Add:member 4 game loop code by Tesnim

---
+++ member4_tesnim.cpp
@@ -0,0 +1,7 @@
+//game loop setup
+int round =1;
+char cont ='y';
+while ( cont == 'y' || cont =='y'){
+std::cout<<"\n - - - round "<< round <<"- - - \n";
+std::cout<<"current money:"<<capital<<"\n";
+}
